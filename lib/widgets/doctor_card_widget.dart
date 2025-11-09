131321fb5892f3f7233adcf3e49c8958e142d2bf
linaaaashraff <ashraflina21@gmail.com>
2025-11-09 19:29:37 +0200

    Edit Doctor Card Widget

--- lib/widgets/doctor_card_widget.dart
+++ lib/widgets/doctor_card_widget.dart
@@ -1,31 +1,47 @@
-
 import 'package:flutter/material.dart';
 import '../models/doctor.dart';
-
+import '../screens/doctor_details_screen.dart';
 
 class DoctorCardWidget extends StatelessWidget {
   final Doctor doctor;
   const DoctorCardWidget({super.key, required this.doctor});
 
-
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      elevation: 3,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      child: ListTile(
-        leading: CircleAvatar(
-          backgroundImage: NetworkImage(doctor.image),
-          radius: 28,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DoctorDetailsScreen(doctor: doctor),
+          ),
+        );
+      },
+
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        elevation: 3,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(doctor.image),
+            radius: 28,
+          ),
+
+          title: Text(
+            doctor.name,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+
+          subtitle: Text(
+            '${doctor.specialty}\n⭐ ${doctor.rating.toString()}',
+          ),
+
+          isThreeLine: true,
         ),
-        title: Text(
-          doctor.name,
-          style: const TextStyle(fontWeight: FontWeight.bold),
-        ),
-        subtitle: Text('${doctor.specialty}\n ${doctor.rating.toString()}'),
-        isThreeLine: true,
+
       ),
+
     );
   }
 }
