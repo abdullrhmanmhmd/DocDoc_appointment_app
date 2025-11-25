eefe293d44c7efcb661c5fa9e3a6d0bdc62f2850
Haidy Omar <haidyomar645@gmail.com>
2025-11-25 21:20:25 +0200

    update profile_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
-import 'package:doc_app_sw/doc_app.dart';
-
 import 'package:flutter/material.dart';
+import 'screens/profile_screen.dart';
+
 void main() {
-  runApp(DocApp());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ProfileScreen(),
+    );
+  }
 }
